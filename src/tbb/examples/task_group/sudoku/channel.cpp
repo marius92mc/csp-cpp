b38d888c9d7cbb794d3d975e0c4fa8e15db5885e
marius92mc <marius92mc@gmail.com>
2018-01-28 21:06:46 +0200

    Add some examples to show `task_group` behaviour

--- src/tbb/examples/task_group/sudoku/channel.cpp
+++ src/tbb/examples/task_group/sudoku/channel.cpp
@@ -74,7 +74,8 @@ public:
        std::this_thread::sleep_for(
            std::chrono::seconds(getRandom() % kSecondsLimit)
        );
-       std::cout << operation << "\n";
+       // TODO uncomment this for debugging, very useful.
+       //std::cout << operation << "\n";
     }
 
 private:
@@ -97,70 +98,149 @@ public:
 };
 
 
-void runExampleWithTaskGroup() {
-    std::string operation;
-    int x;
-    int indexChannel;
+/*
+ * `select` area.
+ * We consider the default case of waiting 10 seconds, when this timer
+ * is reached, we cancel the entire task group.
+ */
+void runSelectInstruction(task_group& tg) {
+  while (true) {
+    if (trigger != 0) {
+      // A pop from a queue was encountered, aka
+      // an item from a channel was received,
+      // more precisely from the channel with index `trigger`,
+      // because of the `trigger.fetch_and_add(indexChannel)` call.
+      std::cout << "Received item from channel " << trigger << ".\n";
 
-    // Instead of `trigger.fetch_and_add(0);`, because it will affect when
-    // we have multiple usages, adding 0 will maintain the different value
-    // different of 0.
-    trigger = 0;
-
-    gQueues.resize(20); // TODO change this
-    task_group tg;
-
-    // queue indices starting with 1, to have effect the fetch_and_add(number)
-    tg.run(Hello("push", 1, 5));
-    tg.run(Hello("push", 1, 8));
-    tg.run(Hello("push", 2, 10));
-    // This pop is hardcoded for the example, but it actually triggers
-    // when the first pop is encountered.
-    tg.run(Hello("pop", 1));
-    //tg.run(Hello("pop", 3));
-
-    tg.run(DefaultTimer(5));
-
-    /*
-     * `select` are.
-     * We consider the default case of waiting 10 seconds, when this timer
-     * is reached, we cancel the entire task group.
-     */
-    while (true) {
-      if (trigger != 0) {
-        // A pop from a queue was encountered, aka
-        // an item from a channel was received,
-        // more precisely from the channel with index `trigger`,
-        // because of the `trigger.fetch_and_add(indexChannel)` call.
-        std::cout << "Received item from channel " << trigger << ".\n";
-
-        // When an item is received, cancel all the other channels.
-        tg.cancel();
-        break;
-      }
-
-      // Handling the `default` case of `select`.
-      // When it finished all the tasks, including the costliest one
-      // represented by the `DefaultTimer()`, we inspect the returned value,
-      // which is from an enum, and the value `1` corresponse to
-      // `complete, // Not cancelled and all tasks in group have completed`,
-      // in which case it means that we have reached the default case, so
-      // we print the message on console and exit from loop.
-      // `task_group` - https://software.intel.com/en-us/node/506287
-      // `enum task_group_status`, as returned value of `wait()` -
-      // 		https://software.intel.com/en-us/node/506289.
-      // enum task_group_status {
-      //     not_complete, // Not cancelled and not all tasks in group have completed.
-      //     complete,     // Not cancelled and all tasks in group have completed
-      //     canceled      // Task group received cancellation request
-      // };
-      if (tg.wait() == 1) {
-        std::cout << "Default case reached.\n";
-        break;
-      }
+      // When an item is received, cancel all the other channels.
+      tg.cancel();
+      break;
     }
 
-    tg.wait();
+    // Handling the `default` case of `select`.
+    // When it finished all the tasks, including the costliest one
+    // represented by the `DefaultTimer()`, we inspect the returned value,
+    // which is from an enum, and the value `1` corresponse to
+    // `complete, // Not cancelled and all tasks in group have completed`,
+    // in which case it means that we have reached the default case, so
+    // we print the message on console and exit from loop.
+    // `task_group` - https://software.intel.com/en-us/node/506287
+    // `enum task_group_status`, as returned value of `wait()` -
+    // 		https://software.intel.com/en-us/node/506289.
+    // enum task_group_status {
+    //     not_complete, // Not cancelled and not all tasks in group have completed.
+    //     complete,     // Not cancelled and all tasks in group have completed
+    //     canceled      // Task group received cancellation request
+    // };
+    if (tg.wait() == 1) {
+      std::cout << "Default case reached.\n";
+      break;
+    }
+  }
+
+  tg.wait();
+}
+
+
+/*
+ * Put default trigger to 0, as untriggered.
+ */
+void setTriggerToDefault() {
+  trigger = 0;
+}
+
+
+void showExampleGeneral() {
+  std::cout << "\n---------- Example - General behaviour ----------\n";
+
+  setTriggerToDefault();
+
+  gQueues.clear();
+  gQueues.resize(20); // TODO change this
+
+  task_group tg;
+
+  // queue indices starting with 1, to have effect the fetch_and_add(number)
+  tg.run(Hello("push", 1, 5));
+  tg.run(Hello("push", 1, 8));
+  tg.run(Hello("push", 2, 10));
+  tg.run(Hello("push", 3, 5));
+
+  const unsigned int randomIndexChannel = (getRandom() % 3) + 1;
+
+  std::cout << "Gonna pop from channel " << randomIndexChannel << ".\n";
+  // This pop is hardcoded for the example, but it actually triggers
+  // when the first pop is encountered.
+  tg.run(Hello("pop", randomIndexChannel));
+  //tg.run(Hello("pop", 4));
+
+  tg.run(DefaultTimer(5));
+
+  runSelectInstruction(tg);
+}
+
+
+void showExamplePopFromEmptyChannel() {
+  std::cout << "\n---------- Example - pop() from empty channel ----------\n";
+
+  setTriggerToDefault();
+
+  gQueues.clear();
+  gQueues.resize(20); // TODO change this
+
+  task_group tg;
+
+  // queue indices starting with 1, to have effect the fetch_and_add(number)
+  tg.run(Hello("push", 1, 5));
+  tg.run(Hello("push", 1, 8));
+  tg.run(Hello("push", 2, 10));
+  tg.run(Hello("push", 3, 5));
+
+  const unsigned int indexChannel = 4;
+
+  std::cout << "Gonna pop from channel " << indexChannel << ".\n";
+  // This pop is hardcoded for the example, but it actually triggers
+  // when the first pop is encountered.
+  tg.run(Hello("pop", indexChannel));
+
+  tg.run(DefaultTimer(5));
+
+  runSelectInstruction(tg);
+}
+
+/*
+ * Triggering the default case of `select` when we don't encounter any pop()
+ * from a channel, aka
+ * when we don't receive any item from a channel after `numberOfSeconds` seconds.
+ */
+void showExampleDefaultCase(const int& numberOfSeconds = 5) {
+  std::cout << "\n---------- Example - reaching default case after "
+            << numberOfSeconds << " seconds ----------\n";
+
+  setTriggerToDefault();
+
+  gQueues.clear();
+  gQueues.resize(20); // TODO change this
+
+  task_group tg;
+
+  // queue indices starting with 1, to have effect the fetch_and_add(number)
+  tg.run(Hello("push", 1, 5));
+  tg.run(Hello("push", 1, 8));
+  tg.run(Hello("push", 2, 10));
+  tg.run(Hello("push", 3, 5));
+
+  // We haven't called any pop(), so the default case will be triggered.
+  tg.run(DefaultTimer(numberOfSeconds));
+
+  runSelectInstruction(tg);
+}
+
+
+void runExampleWithTaskGroup() {
+  showExampleGeneral();
+  showExamplePopFromEmptyChannel();
+  showExampleDefaultCase();
 }
 
 
