60811d7b25c73bad76fa72d8846d850930e23eb9
Marius-Constantin Melemciuc <marius92mc@users.noreply.github.com>
2018-02-02 09:21:44 +0200

    Extend task group (#13)
    * Move `select` behaviour in a class
    
    * Use channel name instead of its index

--- src/tbb/examples/task_group/sudoku/channel.cpp
+++ src/tbb/examples/task_group/sudoku/channel.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <queue>
+#include <unordered_map>
 
 #include "tbb/atomic.h"
 #include "tbb/tick_count.h"
@@ -26,6 +27,8 @@ const unsigned int gKDefaultnumberOfChannels = 20;
  * there's no way count would have different values in different threads.
  */
 atomic<int> trigger;
+std::unordered_map<std::string, int> channelToIndex;
+std::unordered_map<int, std::string> indexToChannel;
 
 
 int getRandom() {
@@ -39,15 +42,20 @@ class Hello {
     const int kSecondsLimit;
 public:
     Hello(
-        std::string operation, int indexChannel, int content = 0
+        std::string operation, const std::string& channelName, int content = 0
     ): kSecondsLimit(5) {
       if (operation != "push" && operation != "pop") {
         std::cout << "Not a valid operation.\n";
         return;
       }
+      if (channelToIndex.find(channelName) == channelToIndex.end()) {
+        std::cout << "On " << operation << " not a valid channel name: " << channelName << ".\n";
+        return;
+      }
+      const int indexChannel = channelToIndex[channelName];
       if (!isValidIndex(indexChannel)) {
         std::cout << "On " << operation
-                  << " not a valid channel: " << indexChannel << ".\n";
+                  << " not a valid channel index: " << indexChannel << ".\n";
         return;
       }
 
@@ -57,7 +65,7 @@ public:
 
       if (operation == "pop") {
         if (gQueues[indexChannel].size() < 1) {
-          std::cout << "Can't pop on empty channel " << indexChannel << ".\n";
+          std::cout << "Can't pop on empty channel " << channelName << ".\n";
           return;
         }
         gQueues[indexChannel].pop();
@@ -98,47 +106,58 @@ public:
 
 /*
  * `select` area.
- * We consider the default case of waiting 10 seconds, when this timer
+ * We consider the default case of waiting 5 seconds, when this timer
  * is reached, we cancel the entire task group.
  */
-void runSelectInstruction(task_group& tg) {
-  while (true) {
-    if (trigger != 0) {
-      // A pop from a queue was encountered, aka
-      // an item from a channel was received,
-      // more precisely from the channel with index `trigger`,
-      // because of the `trigger.fetch_and_add(indexChannel)` call.
-      std::cout << "Received item from channel " << trigger << ".\n";
-
-      // When an item is received, cancel all the other channels.
-      tg.cancel();
-      break;
-    }
-
-    // Handling the `default` case of `select`.
-    // When it finished all the tasks, including the costliest one
-    // represented by the `DefaultTimer()`, we inspect the returned value,
-    // which is from an enum, and the value `1` corresponse to
-    // `complete, // Not cancelled and all tasks in group have completed`,
-    // in which case it means that we have reached the default case, so
-    // we print the message on console and exit from loop.
-    // `task_group` - https://software.intel.com/en-us/node/506287
-    // `enum task_group_status`, as returned value of `wait()` -
-    // 		https://software.intel.com/en-us/node/506289.
-    // enum task_group_status {
-    //     not_complete, // Not cancelled and not all tasks in group have completed.
-    //     complete,     // Not cancelled and all tasks in group have completed
-    //     canceled      // Task group received cancellation request
-    // };
-    if (tg.wait() == 1) {
-      std::cout << "Default case reached.\n";
-      break;
-    }
+class SelectInstruction {
+public:
+  SelectInstruction() {
   }
 
-  tg.wait();
-}
+  ~SelectInstruction() {
+  }
 
+  void execute(task_group& tg) {
+    while (true) {
+      if (trigger != 0) {
+        // A pop from a queue was encountered, aka
+        // an item from a channel was received,
+        // more precisely from the channel with index `trigger`,
+        // because of the `trigger.fetch_and_add(indexChannel)` call.
+        if (indexToChannel.find(trigger) == indexToChannel.end()) {
+          std::cout << "Trigger not set properly.\n";
+        }
+        std::cout << "Received item from channel " << indexToChannel[trigger] << ".\n";
+
+        // When an item is received, cancel all the other channels.
+        tg.cancel();
+        break;
+      }
+
+      // Handling the `default` case of `select`.
+      // When it finished all the tasks, including the costliest one
+      // represented by the `DefaultTimer()`, we inspect the returned value,
+      // which is from an enum, and the value `1` corresponse to
+      // `complete, // Not cancelled and all tasks in group have completed`,
+      // in which case it means that we have reached the default case, so
+      // we print the message on console and exit from loop.
+      // `task_group` - https://software.intel.com/en-us/node/506287
+      // `enum task_group_status`, as returned value of `wait()` -
+      //    https://software.intel.com/en-us/node/506289.
+      // enum task_group_status {
+      //     not_complete, // Not cancelled and not all tasks in group have completed.
+      //     complete,     // Not cancelled and all tasks in group have completed
+      //     canceled      // Task group received cancellation request
+      // };
+      if (tg.wait() == complete) {
+        std::cout << "Default case reached.\n";
+        break;
+      }
+    }
+
+    tg.wait();
+  }
+};
 
 /*
  * Put default trigger to 0, as untriggered.
@@ -148,9 +167,25 @@ void setTriggerToDefault() {
 }
 
 
+void setHashTables() {
+  channelToIndex.clear();
+  indexToChannel.clear();
+
+  channelToIndex["channel1"] = 1;
+  channelToIndex["channel2"] = 2;
+  channelToIndex["channel3"] = 3;
+  indexToChannel[1] = "channel1";
+  indexToChannel[2] = "channel2";
+  indexToChannel[3] = "channel3";
+}
+
+
 void showExampleGeneral() {
   std::cout << "\n---------- Example - General behaviour ----------\n";
 
+  // IMPORTANT
+  // We have to hardcode this statement everytime in the injected code area, because this is the place where it works to be, 
+  // in order to have the desired behaviour. 
   setTriggerToDefault();
 
   gQueues.clear();
@@ -158,23 +193,27 @@ void showExampleGeneral() {
 
   task_group tg;
 
+  setHashTables();
+
   // queue indices starting with 1, to have effect the fetch_and_add(number)
-  tg.run(Hello("push", 1, 5));
-  tg.run(Hello("push", 1, 8));
-  tg.run(Hello("push", 2, 10));
-  tg.run(Hello("push", 3, 5));
+  tg.run(Hello("push", "channel1", 5));
+  tg.run(Hello("push", "channel1", 8));
+  tg.run(Hello("push", "channel2", 10));
+  tg.run(Hello("push", "channel3", 5));
 
-  const unsigned int randomIndexChannel = (getRandom() % 3) + 1;
+  std::vector<std::string> channelNames = {"channel1", "channel2", "channel3"};
+  const std::string randomChannelName = channelNames[getRandom() % 3]; 
 
-  std::cout << "Gonna pop from channel " << randomIndexChannel << ".\n";
+  std::cout << "Gonna pop from channel " << randomChannelName << ".\n";
   // This pop is hardcoded for the example, but it actually triggers
   // when the first pop is encountered.
-  tg.run(Hello("pop", randomIndexChannel));
-  //tg.run(Hello("pop", 4));
+  tg.run(Hello("pop", randomChannelName));
 
   tg.run(DefaultTimer(5));
 
-  runSelectInstruction(tg);
+
+  SelectInstruction selectInstruction;
+  selectInstruction.execute(tg);
 }
 
 
@@ -188,22 +227,26 @@ void showExamplePopFromEmptyChannel() {
 
   task_group tg;
 
+
+  setHashTables();
+  channelToIndex["channel4"] = 4;
+  indexToChannel[4] = "channel4";
+
   // queue indices starting with 1, to have effect the fetch_and_add(number)
-  tg.run(Hello("push", 1, 5));
-  tg.run(Hello("push", 1, 8));
-  tg.run(Hello("push", 2, 10));
-  tg.run(Hello("push", 3, 5));
+  tg.run(Hello("push", "channel1", 5));
+  tg.run(Hello("push", "channel1", 8));
+  tg.run(Hello("push", "channel2", 10));
+  tg.run(Hello("push", "channel3", 5));
 
-  const unsigned int indexChannel = 4;
-
-  std::cout << "Gonna pop from channel " << indexChannel << ".\n";
+  std::cout << "Gonna pop from channel channel4.\n";
   // This pop is hardcoded for the example, but it actually triggers
   // when the first pop is encountered.
-  tg.run(Hello("pop", indexChannel));
+  tg.run(Hello("pop", "channel4"));
 
   tg.run(DefaultTimer(5));
 
-  runSelectInstruction(tg);
+  SelectInstruction selectInstruction;
+  selectInstruction.execute(tg);
 }
 
 /*
@@ -222,16 +265,19 @@ void showExampleDefaultCase(const int& numberOfSeconds = 5) {
 
   task_group tg;
 
+  setHashTables();
+
   // queue indices starting with 1, to have effect the fetch_and_add(number)
-  tg.run(Hello("push", 1, 5));
-  tg.run(Hello("push", 1, 8));
-  tg.run(Hello("push", 2, 10));
-  tg.run(Hello("push", 3, 5));
+  tg.run(Hello("push", "channel1", 5));
+  tg.run(Hello("push", "channel1", 8));
+  tg.run(Hello("push", "channel2", 10));
+  tg.run(Hello("push", "channel3", 5));
 
   // We haven't called any pop(), so the default case will be triggered.
   tg.run(DefaultTimer(numberOfSeconds));
 
-  runSelectInstruction(tg);
+  SelectInstruction selectInstruction;
+  selectInstruction.execute(tg);
 }
 
 
@@ -245,20 +291,23 @@ void showExampleNotAValidChannel() {
 
   task_group tg;
 
-  // queue indices starting with 1, to have effect the fetch_and_add(number)
-  tg.run(Hello("push", 1, 5));
-  tg.run(Hello("push", 1, 8));
-  tg.run(Hello("push", 2, 10));
-  tg.run(Hello("push", 3, 5));
+  setHashTables();
 
-  tg.run(Hello("push", gKDefaultnumberOfChannels + 1, 1));
-  tg.run(Hello("pop", gKDefaultnumberOfChannels + 2, 2));
+  // queue indices starting with 1, to have effect the fetch_and_add(number)
+  tg.run(Hello("push", "channel1", 5));
+  tg.run(Hello("push", "channel1", 8));
+  tg.run(Hello("push", "channel2", 10));
+  tg.run(Hello("push", "channel3", 5));
+
+  tg.run(Hello("push", "inexistentChannel1", 1));
+  tg.run(Hello("pop", "inexistentChannel2", 2));
 
 
   // We haven't called any pop(), so the default case will be triggered.
   tg.run(DefaultTimer(5));
 
-  runSelectInstruction(tg);
+  SelectInstruction selectInstruction;
+  selectInstruction.execute(tg);
 }
 
 
