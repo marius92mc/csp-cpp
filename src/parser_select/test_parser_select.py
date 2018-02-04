93778e1316180a4e925b5a911bcbf08256fe7225
marius92mc <marius92mc@gmail.com>
2018-02-04 15:24:00 +0200

    Add more unit-tests

--- src/parser_select/test_parser_select.py
+++ src/parser_select/test_parser_select.py
@@ -49,3 +49,91 @@ def test_tmpdir(tmpdir: local.LocalPath) -> None:
     assert p.read() == file_content
 
 
+def _get_file_name() -> str:
+    return "hello.cpp"
+
+
+def _get_file_path(tmpdir: local.LocalPath) -> str:
+    return str(tmpdir.join(_get_file_name()))
+
+
+def _add_content(tmpdir: local.LocalPath, content: str) -> local.LocalPath:
+    p = tmpdir.join(_get_file_name())
+    p.write_text(content, encoding="utf-16-le")
+    return p
+
+
+def test_empty_content(tmpdir: local.LocalPath) -> None:
+    _add_content(tmpdir, "#include <iostream>\n")
+    select_data = SelectParser(_get_file_path(tmpdir), "").parse()
+    assert select_data == []
+
+
+@pytest.fixture
+def cpp_includes() -> str:
+    return '#include <iostream>\n' \
+           '\n' \
+           'using namespace std;\n' \
+           '\n' \
+
+
+def test_empty_select(tmpdir: local.LocalPath, cpp_includes: str) -> None:
+    _add_content(tmpdir, cpp_includes +
+                         "int main() {\n"
+                         "  select {\n"
+                         "  }\n"
+                         "}\n")
+    select_data = SelectParser(_get_file_path(tmpdir), "").parse()
+    assert select_data == [[]]
+
+
+def test_select_with_one_case(tmpdir: local.LocalPath, cpp_includes: str) -> None:
+    _add_content(tmpdir, cpp_includes +
+                         'int main() {\n'
+                         '    select {\n' +
+                         '      case message1 <- channel1:\n'
+                         '      {\n'
+                         '          std::cout << "1";\n'
+                         '          std::cout << "2";\n'
+                         '      }\n'
+                         '  }\n'
+                         '}\n')
+    select_data = SelectParser(_get_file_path(tmpdir), "").parse()
+    assert select_data == [
+        [
+            (
+                'message1',
+                'channel1',
+                '      {\n'
+                '          std::cout << "1";\n'
+                '          std::cout << "2";\n'
+                '      }\n'
+            )
+        ]
+    ]
+
+
+def test_select_with_just_one_default(tmpdir: local.LocalPath, cpp_includes: str) -> None:
+    _add_content(tmpdir, cpp_includes +
+                         'int main() {\n'
+                         '    select {\n' +
+                         '      default:\n'
+                         '      {\n'
+                         '          std::cout << "1";\n'
+                         '          std::cout << "2";\n'
+                         '      }\n'
+                         '  }\n'
+                         '}\n')
+    select_data = SelectParser(_get_file_path(tmpdir), "").parse()
+    assert select_data == [
+        [
+            (
+                 None,
+                'default',
+                '      {\n'
+                '          std::cout << "1";\n'
+                '          std::cout << "2";\n'
+                '      }\n'
+            )
+        ]
+    ]
